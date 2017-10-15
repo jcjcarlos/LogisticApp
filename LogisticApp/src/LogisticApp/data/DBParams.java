bbb32054b27b0f6698bd4cd5e5e2488e729765a8
Caio Jordão Carvalho <cjlcarvalho@live.com>
2017-10-15 17:52:56 -0300

    LogisticApp

--- LogisticApp/src/LogisticApp/data/DBParams.java
+++ LogisticApp/src/LogisticApp/data/DBParams.java
@@ -16,88 +16,12 @@ public class DBParams {
 	private static String USER = null;
 	private static String PWD = null;
 	
-	private static BufferedReader openReader() throws IOException {
-		return new BufferedReader(new FileReader("dbconfigs.txt"));
-	}
-	
-	public static String getDRIVER() throws IOException {
-		if(DRIVER == null){
-			BufferedReader reader = DBParams.openReader();
-			String line = reader.readLine().trim();
-			while(line != null){
-				if(line.startsWith("DRIVER:")){
-					DRIVER = line.substring(7, line.length()) + ":";
-					break;
-				}
-				line = reader.readLine().trim();
-			}
-			reader.close();
-		}
-		return DRIVER;
-	}
-	
-	public static String getSGDB() throws IOException {
-		if(SGBD == null){
-			BufferedReader reader = DBParams.openReader();
-			String line = reader.readLine().trim();
-			while(line != null){
-				if(line.startsWith("SGBD:")){
-					SGBD = line.substring(5, line.length()) + ":";
-					break;
-				}
-				line = reader.readLine().trim();
-			}
-			reader.close();
-		}
-		return SGBD;
-	}
-
-	public static String getHOST() throws IOException {
-		if(HOST == null){
-			BufferedReader reader = DBParams.openReader();
-			String line = reader.readLine().trim();
-			while(line != null){
-				if(line.startsWith("HOST:")){
-					HOST = "//" + line.substring(5, line.length());
-					break;
-				}
-				line = reader.readLine().trim();
-			}
-			reader.close();
-		}
-		return HOST;
-	}
-
-	public static String getPORT() throws IOException {
-		if(PORT == null){
-			BufferedReader reader = DBParams.openReader();
-			String line = reader.readLine().trim();
-			while(line != null){
-				if(line.startsWith("PORT:")){
-					PORT = ":" + line.substring(5, line.length());
-					break;
-				}
-				line = reader.readLine().trim();
-			}
-			reader.close();
-		}
-		return PORT;
-	}
-
-	public static String getDATABASE() throws IOException {
-		if(DATABASE == null){
-			BufferedReader reader = DBParams.openReader();
-			String line = reader.readLine().trim();
-			while(line != null){
-				if(line.startsWith("DATABASE:")){
-					DATABASE = "/" + line.substring(9, line.length());
-					break;
-				}
-				line = reader.readLine().trim();
-			}
-			reader.close();
-		}
-		return DATABASE;
+	public static String getURI throws IOException {
+		return String.format("%1$s:%2$s://%3$s:%4$s/%5$s", DBParams.getDRIVER(),
+				     				   DBParams.getSGBD(),
+				     				   DBParams.getHOST(),
+				     				   DBParams.getPORT(),
+				     				   DBParams.getDATABASE());
 	}
 
 	public static String getUSER() throws IOException {
@@ -130,6 +54,90 @@ public class DBParams {
 			reader.close();
 		}
 		return PWD;
+	}
+	
+	private static BufferedReader openReader() throws IOException {
+		return new BufferedReader(new FileReader("dbconfigs.txt"));
+	}
+	
+	private static String getDRIVER() throws IOException {
+		if(DRIVER == null){
+			BufferedReader reader = DBParams.openReader();
+			String line = reader.readLine().trim();
+			while(line != null){
+				if(line.startsWith("DRIVER:")){
+					DRIVER = line.substring(7, line.length());
+					break;
+				}
+				line = reader.readLine().trim();
+			}
+			reader.close();
+		}
+		return DRIVER;
+	}
+	
+	private static String getSGDB() throws IOException {
+		if(SGBD == null){
+			BufferedReader reader = DBParams.openReader();
+			String line = reader.readLine().trim();
+			while(line != null){
+				if(line.startsWith("SGBD:")){
+					SGBD = line.substring(5, line.length());
+					break;
+				}
+				line = reader.readLine().trim();
+			}
+			reader.close();
+		}
+		return SGBD;
+	}
+
+	private static String getHOST() throws IOException {
+		if(HOST == null){
+			BufferedReader reader = DBParams.openReader();
+			String line = reader.readLine().trim();
+			while(line != null){
+				if(line.startsWith("HOST:")){
+					HOST = line.substring(5, line.length());
+					break;
+				}
+				line = reader.readLine().trim();
+			}
+			reader.close();
+		}
+		return HOST;
+	}
+
+	private static String getPORT() throws IOException {
+		if(PORT == null){
+			BufferedReader reader = DBParams.openReader();
+			String line = reader.readLine().trim();
+			while(line != null){
+				if(line.startsWith("PORT:")){
+					PORT = line.substring(5, line.length());
+					break;
+				}
+				line = reader.readLine().trim();
+			}
+			reader.close();
+		}
+		return PORT;
+	}
+
+	private static String getDATABASE() throws IOException {
+		if(DATABASE == null){
+			BufferedReader reader = DBParams.openReader();
+			String line = reader.readLine().trim();
+			while(line != null){
+				if(line.startsWith("DATABASE:")){
+					DATABASE = line.substring(9, line.length());
+					break;
+				}
+				line = reader.readLine().trim();
+			}
+			reader.close();
+		}
+		return DATABASE;
 	}
 
 }
