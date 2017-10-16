146c0fc3fc90792c9f52a6e763a158361b925308
Caio Jordão Carvalho <cjlcarvalho@live.com>
2017-10-16 11:53:05 -0300

    LogisticApp - Changing ContratacaoTransporte

--- LogisticApp/src/LogisticApp/business/session/ContratacaoTransporte.java
+++ LogisticApp/src/LogisticApp/business/session/ContratacaoTransporte.java
@@ -12,77 +12,47 @@ import LogisticApp.data.sql.RotaDAOSQL;
 
 public class ContratacaoTransporte implements IContratacaoTransporteSession {
 
-	private Localidade destino;
-	private Localidade origem;
-	private double pesoVolume;
 	private IRotaDAO rotaDAO;
 
-	public ContratacaoTransporte(Localidade origem, Localidade destino, double pesoVolume) {
-		this.setOrigem(origem);
-		this.setDestino(destino);
-		this.setPesoVolume(pesoVolume);
+	public ContratacaoTransporte() {
 		this.rotaDAO = new RotaDAOSQL();
 	}
 
 	@Override
-	public void atualizarRota(int idRota) throws Exception {
+	public void atualizarRota(int idRota, double pesoVolume) throws Exception {
 		Rota rota = this.rotaDAO.retrieveById(idRota);
-		rota.aumentarCapacidadeAlocada(this.getPesoVolume());
+		rota.aumentarCapacidadeAlocada(pesoVolume);
 		this.rotaDAO.update(rota);
 	}
 
-	private double calcularValorPeso(double custoGrama) {
-		return this.getPesoVolume() * 1000 * custoGrama;
-	}
-
-	public Localidade getDestino() {
-		return this.destino;
+	private double calcularValorPeso(double custoGrama, double pesoVolume) {
+		return pesoVolume * 1000 * custoGrama;
 	}
 
 	@Override
-	public Map<Integer, String> getInfoRotasCapacitadas() throws Exception {
+	public Map<Integer, String> getInfoRotasCapacitadas(Localidade origem, Localidade destino, double pesoVolume) throws Exception {
 		Map<Integer, String> rotasCapacitadas = new HashMap<Integer, String>();
-		Collection<Rota> rotas = this.getRotasCapacitadas();
+		Collection<Rota> rotas = this.getRotasCapacitadas(origem, destino, pesoVolume);
 		for (Rota rota : rotas)
 			rotasCapacitadas.put(rota.getId(), mensagemInfoRota(rota));
 		return rotasCapacitadas;
 	}
 
-	public Localidade getOrigem() {
-		return this.origem;
-	}
-
-	public double getPesoVolume() {
-		return this.pesoVolume;
-	}
-
-	private Collection<Rota> getRotasCapacitadas() throws Exception {
-		Collection<Rota> rotas = this.rotaDAO.retrieveByOriginDestiny(this.getOrigem(), this.getDestino());
+	private Collection<Rota> getRotasCapacitadas(Localidade origem, Localidade destino, double pesoVolume) throws Exception {
+		Collection<Rota> rotas = this.rotaDAO.retrieveByOriginDestiny(origem, destino);
 		for (Rota rota : rotas) {
-			if (rota.getCapacidadeTransporte() < this.getPesoVolume())
+			if (rota.getCapacidadeTransporte() < pesoVolume)
 				rotas.remove(rota);
 		}
 		return rotas;
 	}
 
-	private String mensagemInfoRota(Rota rota) {
+	private String mensagemInfoRota(Rota rota, double pesoVolume) {
 		String result = "";
 		result += rota.getNome() + " - ";
 		result += rota.getTempoEntrega() + " dias - R$ ";
-		result += String.format("%.2f", this.calcularValorPeso(rota.getCustoGrama()));
+		result += String.format("%.2f", this.calcularValorPeso(rota.getCustoGrama(), pesoVolume));
 		return result;
-	}
-
-	public void setDestino(Localidade destino) {
-		this.destino = destino;
-	}
-
-	public void setOrigem(Localidade origem) {
-		this.origem = origem;
-	}
-
-	public void setPesoVolume(double pesoVolume) {
-		this.pesoVolume = pesoVolume;
 	}
 
 }
